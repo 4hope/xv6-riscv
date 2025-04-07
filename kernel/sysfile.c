86b7b9c12682b3fa61f0f706fb5a6bd6c1af6f8e
4hope <no278149@gmail.com>
2025-04-07 18:50:38 +0300

    last fix

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -549,9 +549,17 @@ sys_mutex_unlock(void)
 
   f = myproc()->ofile[fd];
 
-  if (fd < 0 || f == 0 || f->type != FD_MUTEX || f->mutex->pid != myproc()->pid)
+  if (fd < 0 || f == 0 || f->type != FD_MUTEX)
     return -1;
 
+  acquire(&f->mutex->lk);
+
+  if (f->mutex->pid != myproc()->pid) {
+    release(&f->mutex->lk);
+    return -1;
+  }
+
+  release(&f->mutex->lk);
   releasesleep(f->mutex);
 
   return fd;
